ce7cfde8d2ceb77590f793d3321767cb4e4c35a1
David <David>
2021-09-05 15:28:17 -0700

    t

--- ExternalIO/bankers-bonus-client.cpp
+++ ExternalIO/bankers-bonus-client.cpp
@@ -1,4 +1,34 @@
-
+/*
+ * Demonstrate external client inputing and receiving outputs from a SPDZ process,
+ * following the protocol described in https://eprint.iacr.org/2015/1006.pdf.
+ *
+ * Provides a client to bankers_bonus.mpc program to calculate which banker pays for lunch based on
+ * the private value annual bonus. Up to 8 clients can connect to the SPDZ engines running
+ * the bankers_bonus.mpc program.
+ *
+ * Each connecting client:
+ * - sends an increasing id to identify the client, starting with 0
+ * - sends an integer (0 meaining more players will join this round or 1 meaning stop the round and calc the result).
+ * - sends an integer input (bonus value to compare)
+ *
+ * The result is returned authenticated with a share of a random value:
+ * - share of winning unique id [y]
+ * - share of random value [r]
+ * - share of winning unique id * random value [w]
+ *   winning unique id is valid if ∑ [y] * ∑ [r] = ∑ [w]
+ *
+ * To run with 2 parties / SPDZ engines:
+ *   ./Scripts/setup-online.sh to create triple shares for each party (spdz engine).
+ *   ./Scripts/setup-clients.sh to create SSL keys and certificates for clients
+ *   ./compile.py bankers_bonus
+ *   ./Scripts/run-online.sh bankers_bonus to run the engines.
+ *
+ *   ./bankers-bonus-client.x 0 2 100 0
+ *   ./bankers-bonus-client.x 1 2 200 0
+ *   ./bankers-bonus-client.x 2 2 50 1
+ *
+ *   Expect winner to be second client with id 1.
+ */
 
 #include "Math/gfp.h"
 #include "Math/gf2n.h"
@@ -61,9 +91,6 @@ void send_private_inputs(const vector<T>& values, vector<ssl_socket*>& sockets, 
     for (int i = 0; i < num_inputs; i++)
     {
         T y = values[i] + triples[i][0];
-	cout << values[i];
-	cout << triples[i][0];
-	cout << "Next";
         y.pack(os);
     }
     for (int j = 0; j < nparties; j++)
@@ -98,79 +125,62 @@ T receive_result(vector<ssl_socket*>& sockets, int nparties)
 }
 
 template<class T>
-void one_run(vector<T>& values, vector<ssl_socket*>& sockets, int nparties)
+void one_run(T salary_value, vector<ssl_socket*>& sockets, int nparties)
 {
     // Run the computation
-
-    send_private_inputs<T>(values, sockets, nparties);
+    send_private_inputs<T>({salary_value}, sockets, nparties);
     cout << "Sent private inputs to each SPDZ engine, waiting for result..." << endl;
 
     // Get the result back (client_id of winning client)
-    T result1 = receive_result<T>(sockets, nparties);
-    //T result2 = receive_result<T>(sockets, nparties);
-    //T result3 = receive_result<T>(sockets, nparties);
+    T result = receive_result<T>(sockets, nparties);
 
-    cout << "Accuracy is : " << result1 << endl;
+    cout << "Winning client id is : " << result << endl;
 }
 
 template<class T>
-void run(vector<T>& values, vector<ssl_socket*>& sockets, int nparties)
+void run(double salary_value, vector<ssl_socket*>& sockets, int nparties)
 {
     // sint
-    //one_run<T>(long(round(salary_value)), sockets, nparties);
+    one_run<T>(long(round(salary_value)), sockets, nparties);
     // sfix with f = 16
-    //one_run<T>(long(round(salary_value * exp2(16))), sockets, nparties);
-    cout << "Run \n";
-    for(std::vector<Z2<64> >::size_type i=0; i<values.size(); ++i)
-           std::cout << values[i] << ' ';
-    one_run<T>(values, sockets, nparties);
-
-
+    one_run<T>(long(round(salary_value * exp2(16))), sockets, nparties);
 }
 
 int main(int argc, char** argv)
 {
     int my_client_id;
     int nparties;
+    double salary_value;
     int finish;
     int port_base = 14000;
 
+    if (argc < 5) {
+        cout << "Usage is bankers-bonus-client <client identifier> <number of spdz parties> "
+           << "<salary to compare> <finish (0 false, 1 true)> <optional host names..., default localhost> "
+           << "<optional spdz party port base number, default 14000>" << endl;
+        exit(0);
+    }
 
     my_client_id = atoi(argv[1]);
     nparties = atoi(argv[2]);
-    finish = atoi(argv[3]);
+    salary_value = atof(argv[3]);
+    finish = atoi(argv[4]);
     vector<const char*> hostnames(nparties, "localhost");
 
-    vector<Z2<64>> data;
-
-    std::fstream myfile("data.txt", std::ios_base::in);
-
-    double a;
-    while (myfile >> a)
+    if (argc > 5)
     {
-        data.push_back(long(round(a)));
-    }
-
-    for(std::vector<Z2<64> >::size_type i=0; i<data.size(); ++i)
-           std::cout << data[i] << ' ';
-
-    //for (int x : labels_inputs)
-    //        data.push_back(x);
-
-    if (argc > 4)
-    {
-        if (argc < 4 + nparties)
+        if (argc < 5 + nparties)
         {
             cerr << "Not enough hostnames specified";
             exit(1);
         }
 
         for (int i = 0; i < nparties; i++)
-            hostnames[i] = argv[4 + i];
+            hostnames[i] = argv[5 + i];
     }
 
-    if (argc > 4 + nparties)
-        port_base = atoi(argv[4 + nparties]);
+    if (argc > 5 + nparties)
+        port_base = atoi(argv[5 + nparties]);
 
     bigint::init_thread();
 
@@ -201,7 +211,7 @@ int main(int argc, char** argv)
     {
         gfp::init_field(specification.get<bigint>());
         cerr << "using prime " << gfp::pr() << endl;
-        //run<gfp>(data, sockets, nparties);
+        run<gfp>(salary_value, sockets, nparties);
         break;
     }
     case 'R':
@@ -210,13 +220,13 @@ int main(int argc, char** argv)
         switch (R)
         {
         case 64:
-            run<Z2<64>>(data, sockets, nparties);
+            run<Z2<64>>(salary_value, sockets, nparties);
             break;
         case 104:
-            //run<Z2<104>>(data, sockets, nparties);
+            run<Z2<104>>(salary_value, sockets, nparties);
             break;
         case 128:
-            //run<Z2<128>>(data, sockets, nparties);
+            run<Z2<128>>(salary_value, sockets, nparties);
             break;
         default:
             cerr << R << "-bit ring not implemented";
